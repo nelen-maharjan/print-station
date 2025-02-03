5e43bef27c56e8aceeaebdef4df76231be46a8e3
Nelen Maharjan <neleen11@gmail.com>
2025-02-03 22:33:44 +0545

    dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,16 @@ import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import { useState } from 'react';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
@@ -32,7 +42,20 @@ const Navbar = () => {
           onClick={() => setShowMenu(true)}
           className="cursor-pointer"
         />
-        {showMenu && <></>}
+        {showMenu && <>
+          <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+        </>}
       </div>
     </div>
   );
