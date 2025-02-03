f3742572dcd01b85ee9fda4bb66f7b392cb68d43
ArjunMHZ <131872587+ArjunMHZ@users.noreply.github.com>
2025-02-03 22:40:45 +0545

    Updated /contact/page.tsx

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -1,33 +1,49 @@
-import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Mail, MapPin, Phone } from "lucide-react";
 
 const ContactInformation = () => {
-    return ( 
+    return (
         <>
-        <Navbar/>
-        <div className="flex pt-8"> 
-            <div className="bg-sky-900">
-                <h1 className="text-gray-200">Contact Information</h1>
-                <p className="text-gray-400 text-sm">Feel free to contact us</p>
-                <div className="space-y-4 mt-4">
-                    <p className="flex items-center gap-3 text-gray-300 text-sm"><Phone size={15} fill="white" className="text-gray-500"/> <span>+977 9803214411</span></p>
-                    <p className="flex items-center gap-3 text-gray-300 text-sm"><Mail size={15} fill="white" className="text-gray-500"/> mail@gmail.com</p>
-                    <p className="flex items-center gap-3 text-gray-300 text-sm"><MapPin size={15} fill="white" className="text-gray-500"/> Kathmandu, Nepal</p>
+            <div className="grid md:grid-cols-2 gap-4 py-8 md:mt-16">
+                <div className="bg-sky-900 md:rounded-r-3xl md:mr-[13rem]">
+                    <div className="py-14 pl-24">
+                        <h1 className="text-gray-200 text-2xl py-3">Contact Information</h1>
+                        <p className="text-gray-400 text-sm py-2">Feel free to contact us</p>
+                        <div className="space-y-8 mt-6">
+                            <p className="flex items-center gap-3 text-gray-300 text-sm"><Phone size={15} fill="white" className="text-gray-500" /> <span>+977 9803214411</span></p>
+                            <p className="flex items-center gap-3 text-gray-300 text-sm"><Mail size={15} fill="white" className="text-gray-500" /> mail@gmail.com</p>
+                            <p className="flex items-center gap-3 text-gray-300 text-sm"><MapPin size={15} fill="white" className="text-gray-500" /> Kathmandu, Nepal</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-8">
+                    <form action="">
+                        <h1 className="text-lg font-light">Get in Touch With Us</h1>
+                        <div className="relative mt-4 space-x-0.5 md:w-3/4">
+                            <span className="space-y-0.5">
+                                <p className="text-sky-900 text-xs">Full Name</p>
+                                <Input className="border-sky-700" />
+                            </span>
+                            <span className="space-y-0.5">
+                                <p className="text-sky-900 text-xs">Email</p>
+                                <Input className="border-blue-500" />
+                            </span>
+                            <span className="space-y-0.5">
+                                <p className="text-sky-900 text-xs">Phone Number</p>
+                                <Input className="border-blue-500" />
+                            </span>
+                            <span className="space-y-0.5">
+                                <p className="text-sky-900 text-xs">Message</p>
+                                <Input className="border-blue-500" />
+                            </span>
+                            <Button variant='sendMessage' className="absolute right-0 mt-4">Send Message</Button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div>  
-                <form action="">
-                    <h1 className="text-lg font-light">Get in Touch With Us</h1>
-                    <Input type="text" placeholder="anme"/>
-                    <Button variant='destructive'>Send Message</Button>
-                </form>
-            </div>
-        </div>
         </>
- 
-     );
+    );
 }
- 
+
 export default ContactInformation;
